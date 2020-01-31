c4049a273f09b4e96d3ac185e0640ca57217fefc
BAGAI <johnny52@yandex.ru>
2020-01-31 15:49:31 +0500

    добавляем экран авторизации с кнопкой входа

---
+++ app/src/main/java/pet/project/menubuilder/fragment/FragmentGoogleAuth.kt
@@ -0,0 +1,19 @@
+package pet.project.menubuilder.fragment
+
+import android.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pet.project.menubuilder.R
+
+class FragmentGoogleAuth: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater?.inflate(R.layout.fragment_google_auth ,null)
+    }
+}
